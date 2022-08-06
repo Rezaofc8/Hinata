cb14023b19f2311f670264a5fbec513ba9a38427
AyGemuy <91945119+AyGemuy@users.noreply.github.com>
2022-08-07 05:55:53 +0800

    New
    New

--- plugins/tools-nobg.js
+++ plugins/tools-nobg.js
@@ -1,25 +1,6 @@
-/*
-import uploadImage from '../lib/uploadImage.js'
-import { webp2png } from '../lib/webp2mp4.js'
-
-let handler = async (m, { conn, usedPrefix, command }) => {
-	let q = m.quoted ? m.quoted : m,
-		mime = (q.msg || q).mimetype || q.mediaType || ''
-	if (/image/g.test(mime)) {
-		let img = await webp2png(await q.download()),
-			url = API('violetics', '/api/media/removebg', { img }, 'apikey')
-		conn.sendMessage(m.chat, { image: { url }}, { quoted: m })
-	} else throw `Send/reply an image with command ${usedPrefix + command}`
-}
-handler.help = ['removebg']
-handler.tags = ['tools']
-handler.command = /^(no|remove)bg$/i
-
-export default handler
-*/
-
 import uploadFile from '../lib/uploadFile.js'
 import uploadImage from '../lib/uploadImage.js'
+import { webp2png } from '../lib/webp2mp4.js'
 import fetch from 'node-fetch'
 
 let handler = async (m, { conn, usedPrefix, command }) => {
@@ -31,7 +12,7 @@ let name = await conn.getName(who)
   if (!mime) throw 'No media found'
   let media = await q.download()
   let isTele = /image\/(png|jpe?g|gif)|video\/mp4/.test(mime)
-  let link = await (isTele ? uploadImage : uploadFile)(media)
+  let link = await (isTele ? uploadImage : uploadFile : webp2png)(media)
   let url = API('violetics', '/api/media/removebg', { link }, 'apikey')
   let caption = `📮 *L I N K :*
 ${link}
@@ -39,8 +20,7 @@ ${link}
 📛 *E x p i r e d :* ${isTele ? 'No Expiry Date' : 'Unknown'}
 
 *S H O R T :* ${await shortUrl(link)}`
-
-conn.sendButton(m.chat, caption, wm, await(await fetch(link)).buffer(), [['🎀 Menu', '/menu']], m, { fileLength: fsizedoc, seconds: fsizedoc, contextInfo: {
+await conn.sendButton(m.chat, caption, wm, await(await fetch(url)).buffer(), [['🎀 Menu', '/menu']], m, { fileLength: fsizedoc, seconds: fsizedoc, contextInfo: {
           externalAdReply :{
     mediaUrl: sig,
     mediaType: 2,
@@ -48,7 +28,7 @@ conn.sendButton(m.chat, caption, wm, await(await fetch(link)).buffer(), [['🎀 
     title: '👋 Hai, ' + name + ' ' + ucapan,
     body: botdate,
     thumbnail: await(await fetch(pp)).buffer(),
-    sourceUrl: sig
+    sourceUrl: link
      }}
   })
 }
