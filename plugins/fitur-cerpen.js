7696423cc6930b80b64f6e60a7641ff61d7800d4
AyGemuy <91945119+AyGemuy@users.noreply.github.com>
2022-08-07 10:40:17 +0800

    New
    New

--- plugins/fitur-cerpen.js
+++ plugins/fitur-cerpen.js
@@ -1,26 +1,49 @@
 import fetch from 'node-fetch'
 let handler = async (m, { text, usedPrefix, command }) => {
+let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+        let pp = await conn.profilePictureUrl(who).catch(_ => hwaifu.getRandom())
+        let name = await conn.getName(who)
 try {
 if (!text) return m.reply(`Contoh penggunaan ${usedPrefix}${command} cinta\nList:\ncinta\nhoror\njawa\nlucu\nperjuangan\nsahabat`)
   let json = await fetch(`https://api-xcoders.xyz/api/random/cerpen/${text}?apikey=xcoders`)
   let jsons = await json.json()
         let x = jsons.result
-  m.reply(`
-Judul *${x.title}*
-_Author:_ ${x.author_name}
-_Url:_ ${x.author_url}
+  let caption = `${htki} *${x.title}* ${htka}
+*${htjava} Author:* ${x.author_name}
+*${htjava} Url:* ${x.author_url}
+
+${cmenuf}
 ${x.story}
-`.trim())
+`.trim()
+                await conn.sendButton(m.chat, caption, wm, Buffer.alloc(0), [['➡️Next Cerpen➡️', `${usedPrefix}${command} ${text}`]], m, { quoted: fgif, mimetype: ddocx, fileName: ucapan, pageCount: fpagedoc, fileLength: fsizedoc, seconds: fsizedoc, jpegThumbnail: Buffer.alloc(0), contextInfo: {
+          externalAdReply :{
+          showAdAttribution: true,
+    mediaUrl: sig,
+    mediaType: 2,
+    description: wm, 
+    title: '👋 Hai, ' + name,
+    body: botdate,
+    thumbnail: await( await fetch(pp)).buffer(),
+    sourceUrl: sgc }}})
   } catch {
 let url = await fetch('https://masgi.herokuapp.com/api/cerpen')
   let cerpen = await url.json()
-let hasil = `
-*Powered By ${global.wm}*
+let hasil = `${htki} Cerpen *${text}* ${htka}
+*${htjava} Powered By:* ${wm}
 
+${cmenuf}
 ${cerpen.data}
 `.trim()
-
-  m.reply(hasil)
+conn.sendButton(m.chat, hasil, wm, Buffer.alloc(0), [['➡️Next Cerpen➡️', `${usedPrefix}${command} ${text}`]], m, { quoted: fgif, mimetype: ddocx, fileName: ucapan, pageCount: fpagedoc, fileLength: fsizedoc, seconds: fsizedoc, jpegThumbnail: Buffer.alloc(0), contextInfo: {
+          externalAdReply :{
+          showAdAttribution: true,
+    mediaUrl: sig,
+    mediaType: 2,
+    description: wm, 
+    title: '👋 Hai, ' + name,
+    body: botdate,
+    thumbnail: await( await fetch(pp)).buffer(),
+    sourceUrl: sgc }}})
 }
 }
 handler.help = ['cerpen'].map(v => v + ' <apa>')
