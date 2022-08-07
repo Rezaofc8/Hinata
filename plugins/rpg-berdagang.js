330118ea63b89105b6ce57b33be46461335dbaa0
AyGemuy <91945119+AyGemuy@users.noreply.github.com>
2022-08-07 18:07:09 +0800

    New
    New

--- plugins/rpg-berdagang.js
+++ plugins/rpg-berdagang.js
@@ -1,54 +1,52 @@
 const cooldown = 28800000
-let handler = async (m, { conn, text, usedPrefix }) => {
-let re = 5000
-let dapat = await re.getRandom()
+let handler = async(m, { conn, text, usedPrefix, command }) => {
   let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+  let user = global.db.data.users[who]
+  let dapat = (Math.floor(Math.random() * 5000))
   if (!who) throw 'Tag salah satu lah, yang kamu ingin berdagang bareng'
-  let ctimer = (new Date - user.lastcode)
+  let ctimer = (new Date - user.lastdagang)
 let _ctimer = (cooldown - ctimer)
 let timers = clockString(_ctimer)
-let user = global.db.data.users[who]
-
   if (user.lastdagang < cooldown) {
   if (4999 > user.money) throw 'Target tidak memiliki modal harap masukkan modal 5000'
   if (4999 > user.money) throw 'kamu tidak memiliki modal harap masukkan modal 5000'
-  user.money -= dapat * 1
- user.money -= dapat * 1
+  let caption = `${htki} BERDAGANG ${htki}\nMohon tunggu kak..\n@${m.sender.split("@")[0]} dan @${who.split("@")[0]} sedang berdagang.. 😅\n\n@${m.sender.split("@")[0]} dan @${who.split("@")[0]} meletakkan modal -${dapat} 😅`
   
-  let caption = `Mohon tunggu kak..\nKamu dan @${who.split("@")[0]} sedang berdagang.. 😅\n\nKamu dan @${who.split("@")[0]} meletakkan modal -${dapat} 😅`
+  let _caption = `Selamat @${m.sender.split("@")[0]} dan @${who.split("@")[0]} mendapatkan money..\n\nPenghasilan dagang @${m.sender.split("@")[0]} didapatkan +5000\n${user.money += 5000} Money @${m.sender.split("@")[0]}\n\nPenghasilan dagang @${who.split("@")[0]} didapatkan +5000\n${user.money += 5000} Money @${who.split("@")[0]}`
   
-    conn.sendButton(m.chat, caption, wm, null, [['Invetory', `${usedPrefix}inv`], ['Profile', `${usedPrefix}profile`]], m, { mentions: conn.parseMention(caption) })
+  let __caption = `${htki} SUKSES ${htki}\nSelamat @${m.sender.split("@")[0]} dan @${who.split("@")[0]} mendapatkan money..\n\nPenghasilan dagang @${m.sender.split("@")[0]} didapatkan +10000\n${user.money += 10000} Money @${m.sender.split("@")[0]}\n\nPenghasilan dagang @${who.split("@")[0]} didapatkan +10000\n${user.money += 10000} Money @${who.split("@")[0]}`
   
-  let _caption = `Selamat kamu dan @${who.split("@")[0]} mendapatkan money..\n\nPenghasilan dagang kamu didapatkan +5000\n${user.money += 5000} Money kamu\n\nPenghasilan dagang @${who.split("@")[0]} didapatkan +5000\n${user.money += 5000} Money @${who.split("@")[0]}`
-  
-  let __caption = `Selamat kamu dan @${who.split("@")[0]} mendapatkan money..\n\nPenghasilan dagang kamu didapatkan +10000\n${user.money += 10000} Money kamu\n\nPenghasilan dagang @${who.split("@")[0]} didapatkan +10000\n${user.money += 10000} Money @${who.split("@")[0]}`
-  
-  setTimeout(() => {
-					conn.sendButton(m.chat, _caption, wm, null, [['Invetory', `${usedPrefix}inv`], ['Profile', `${usedPrefix}profile`]], m, { mentions: conn.parseMention(_caption) })
-					}, 3600000)
-  setTimeout(() => {
-					conn.sendButton(m.chat, _caption, wm, null, [['Invetory', `${usedPrefix}inv`], ['Profile', `${usedPrefix}profile`]], m, { mentions: conn.parseMention(_caption) })
-					}, 7200000)
-  setTimeout(() => {
-					conn.sendButton(m.chat, _caption, wm, null, [['Invetory', `${usedPrefix}inv`], ['Profile', `${usedPrefix}profile`]], m, { mentions: conn.parseMention(_caption) })
-					}, 10800000)
-  setTimeout(() => {
-					conn.sendButton(m.chat, _caption, wm, null, [['Invetory', `${usedPrefix}inv`], ['Profile', `${usedPrefix}profile`]], m, { mentions: conn.parseMention(_caption) })
-					}, 14400000)
-  setTimeout(() => {
-					conn.sendButton(m.chat, _caption, wm, null, [['Invetory', `${usedPrefix}inv`], ['Profile', `${usedPrefix}profile`]], m, { mentions: conn.parseMention(_caption) })
-					}, 18000000)
-  setTimeout(() => {
-					conn.sendButton(m.chat, _caption, wm, null, [['Invetory', `${usedPrefix}inv`], ['Profile', `${usedPrefix}profile`]], m, { mentions: conn.parseMention(_caption) })
-					}, 21600000)
-  setTimeout(() => {
-					conn.sendButton(m.chat, _caption, wm, null, [['Invetory', `${usedPrefix}inv`], ['Profile', `${usedPrefix}profile`]], m, { mentions: conn.parseMention(_caption) })
-					}, 25200000)
-  setTimeout(() => {
-					conn.sendButton(m.chat, __caption, wm, null, [['Invetory', `${usedPrefix}inv`], ['Profile', `${usedPrefix}profile`]], m, { mentions: conn.parseMention(__caption) })
-					}, 28800000)
+  conn.sendButton(m.chat, caption, clockString(60000), null, [
+      ['Invetory', `${usedPrefix}inv`], ['Profile', `${usedPrefix}profile`]
+    ], m, { mentions: conn.parseMention(caption) })
+    
+					setTimeout(() => {
+			conn.sendButton(m.chat, __caption, `SUKSES`, null, [
+      ['Invetory', `${usedPrefix}inv`], ['Profile', `${usedPrefix}profile`]
+    ], m, { mentions: conn.parseMention(__caption) })
+		}, 10800000)
+
+		setTimeout(() => {
+			conn.sendButton(m.chat, _caption, clockString(10800000), null, [
+      ['Invetory', `${usedPrefix}inv`], ['Profile', `${usedPrefix}profile`]
+    ], m, { mentions: conn.parseMention(_caption) })
+		}, 7200000)
+
+		setTimeout(() => {
+			conn.sendButton(m.chat, _caption, clockString(7200000), null, [
+      ['Invetory', `${usedPrefix}inv`], ['Profile', `${usedPrefix}profile`]
+    ], m, { mentions: conn.parseMention(_caption) })
+		}, 3600000)
+
+		setTimeout(() => {
+			conn.sendButton(m.chat, _caption, clockString(3600000), null, [
+      ['Invetory', `${usedPrefix}inv`], ['Profile', `${usedPrefix}profile`]
+    ], m, { mentions: conn.parseMention(_caption) })
+		}, 60000)
 					user.lastdagang = new Date * 1
-} else conn.sendButton(m.chat, `Anda Sudah Berdagang , tunggu\n${timers} lagi..`, wm, null, [['Adventure','.adventure']], m)
+} else conn.sendButton(m.chat, `Anda Sudah Berdagang tunggu\n${timers} lagi..`, botdate, null, [
+      ['Invetory', `${usedPrefix}inv`], ['Profile', `${usedPrefix}profile`]
+    ], m)
 }
 handler.help = ['berdagang'].map(v => v + ' @[tag]')
 handler.tags = ['rpg']
